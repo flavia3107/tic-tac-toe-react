c48ff2e83d32b8bfc6a574dea1dc84619461c2ce
Flavia Kosti <flaviakosti@gmail.com>
2023-12-28 19:32:31 +0100

    Tic-tac-toe-react: Add gameboard component

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,19 @@
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+];
+
+export default function GameBoard() {
+    return (
+        <ol id="game-board">
+            {initialGameBoard.map((row, index) => <li key={index}>
+                <ol>
+                    {row.map((playerSymbol, colIndex) => <li key={colIndex}>
+                        <button>{playerSymbol}</button>
+                    </li>)}
+                </ol>
+            </li>)}
+        </ol>
+    );
+}
