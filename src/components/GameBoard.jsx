8fbdc90c94a2a79080ef7f2b1fedea86b1e9775f
Flavia Kosti <flaviakosti@gmail.com>
2023-12-29 21:01:28 +0100

    Tic-tac-toe-react: Handle state for objects/arrays

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,12 +7,33 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(indexRow, indexColumn, symbolValue) {
+        /**
+         * This approach is NOT Recommended: It would update the original address value immediately
+         * setGameBoard(previousStateBoard => previousStateBoard[indexRow][indexColumn] = symbol);
+         * 
+         * Objects and arrays are reference values => Should NOT MUTATE THEM DIRECTLY,
+         * Instead create a deep copy first
+         */
+
+        /**
+         * This is Strongly recommended practice
+         */
+        setGameBoard(previousState => {
+            const board = [...previousState.map(inner => [...inner])];
+            board[indexRow][indexColumn] = symbolValue;
+            return board;
+        });
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, index) => <li key={index}>
+            {gameBoard.map((row, index) => <li key={index}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                        <button>{playerSymbol}</button>
+                        <button onClick={() => handleSelectSquare(index, colIndex, 'X')}>{playerSymbol}</button>
                     </li>)}
                 </ol>
             </li>)}
