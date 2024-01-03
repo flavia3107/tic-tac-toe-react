bf0ed97ec9ceb4d8607c51c96468745b7c2e75fb
Flavia Kosti <flaviakosti@gmail.com>
2024-01-03 18:41:20 +0100

    Tic-tac-toe-react: Implement deriving state practice

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
         <Player initialName="Player 1" symbol="X" isActive={ activePlayer === 'X'}/>
         <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
       </ol>
-      <GameBoard activePlayerSymbol={activePlayer} onSelectSquare={handleSelectSquare} />
+      <GameBoard turns={gameTurns} onSelectSquare={handleSelectSquare} />
     </div>
     <Log/>
   </main>
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,14 +1,31 @@
-import { useState } from "react";
-
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ];
+import { useState } from 'react';
 
-export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
+    /**
+     * Gameboard is a computed value since is derivied from a state 
+     * (in this case from GameTurns that is managed in the app component)
+     */
+    let gameBoard = initialGameBoard;
+    /**
+     * This is called deriving state, since it's not handling actual state
+     */
+    for (const turn of turns) {
+        const { square, player } = turn;
+        const { row, column } = square;
 
+        gameBoard[row][column] = player;
+    }
+
+    /**
+     * This is not needed anymore, since the logic has been moved to the app component.
+     *  
+     */
+    const [gameBoardState, setGameBoard] = useState(initialGameBoard);
     function handleSelectSquare(indexRow, indexColumn) {
         /**
          * This approach is NOT Recommended: It would update the original address value immediately
@@ -34,7 +51,7 @@ export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
             {gameBoard.map((row, index) => <li key={index}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                        <button onClick={() => handleSelectSquare(index, colIndex)}>{playerSymbol}</button>
+                        <button onClick={() => onSelectSquare(index, colIndex)}>{playerSymbol}</button>
                     </li>)}
                 </ol>
             </li>)}
