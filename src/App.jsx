2bce1bfdcfd721fd466556abc527349eb49a3bca
Flavia Kosti <flaviakosti@gmail.com>
2024-01-04 19:01:43 +0100

    Tic-tac-toe-react: Remove extra state, use derivation

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import GameBoard from './components/GameBoard.jsx';
 import Log from './components/Log.jsx';
 import { useState } from 'react';
 
+function deriveActivePlayer(state) {
+  let currentPlayer = 'X';
+    if (!!state.length && state[0].player === 'X') {
+      currentPlayer = 'O';
+    }
+    return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(row, column) {
-    setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns(prevTurns => {
       /**
        * Not optimal: Merging two states ( activePlayer and gameTurns)
@@ -18,12 +25,12 @@ function App() {
       /**
        * Best practice:
        * Create a new variable instead : currentPlayer
+       * let currentPlayer = 'X';
+       * if (!!state.length && state[0].player === 'X') {
+       *   currentPlayer = 'O';
+       * }
        */
-      let currentPlayer = 'X';
-      if (!!prevTurns.length && prevTurns[0].player === 'X') {
-        currentPlayer = 'O';
-      }
-
+      const currentPlayer = deriveActivePlayer(prevTurns);
       const updatedTurns = [
         { square: { row, column }, player: currentPlayer },
         ...prevTurns
@@ -36,7 +43,7 @@ function App() {
   return <main>
     <div id="game-container">
       <ol id="players" className="highlight-player">
-        <Player initialName="Player 1" symbol="X" isActive={ activePlayer === 'X'}/>
+        <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
         <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
       </ol>
       <GameBoard turns={gameTurns} onSelectSquare={handleSelectSquare} />
