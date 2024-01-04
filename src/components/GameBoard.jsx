64c1062737d23caa7346d64ac1f993a2de59d0e5
Flavia Kosti <flaviakosti@gmail.com>
2024-01-04 19:05:33 +0100

    Tic-tac-toe-react: Add condition to disable buttons

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -51,7 +51,9 @@ export default function GameBoard({ onSelectSquare, turns }) {
             {gameBoard.map((row, index) => <li key={index}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                        <button onClick={() => onSelectSquare(index, colIndex)}>{playerSymbol}</button>
+                        <button onClick={() => onSelectSquare(index, colIndex)} disabled={!!playerSymbol}>
+                            {playerSymbol}
+                        </button>
                     </li>)}
                 </ol>
             </li>)}
