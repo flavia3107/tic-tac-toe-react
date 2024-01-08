595858e64e31edb3bd40d5d5897ff07ce1f89280
Flavia Kosti <flaviakosti@gmail.com>
2024-01-08 19:08:06 +0100

    Updates

--- src/components/Log.jsx
+++
@@ -1,7 +0,0 @@
-export default function Log({turns}) {
-    return (
-        <ol id="log">
-            {turns.map(turn => <li key={`${turn.square.row}${turn.square.column}`}>Player {turn.player} selected ({turn.square.row},{turn.square.column})</li>)}
-        </ol>
-    );
-}
