9858c5e553edefbc897d03290eeac863cd487162
Flavia Kosti <flaviakosti@gmail.com>
2024-01-10 21:23:01 +0100

    Empty

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // needed for deployment with vite
   base: '/tic-tac-toe-react/'
 })
