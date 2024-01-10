9fbf46c04d4dc9c870969a869db027d8c3760478
Flavia Kosti <flaviakosti@gmail.com>
2024-01-10 21:45:26 +0100

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // needed for deployment with vite
-  base: '/tic-tac-toe-react/'
-})
