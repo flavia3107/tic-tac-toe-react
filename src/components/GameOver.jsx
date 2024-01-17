42c679e12360e37127dd74f2f42a5deabaea2776
Flavia Kosti <flaviakosti@gmail.com>
2024-01-17 22:07:56 +0100

    Updates

--- src/components/GameOver.jsx
+++
@@ -1,8 +0,0 @@
-export default function GameOver({winner, onResetGame}) {
-    return <div id="game-over">
-        <h2>Game Over !</h2>
-        {!!winner && <p>{winner} won</p>}
-        {!winner && <p>It&apos;s a draw!</p>}
-        <p><button onClick={onResetGame}>Rematch!</button></p>
-    </div>
-}
